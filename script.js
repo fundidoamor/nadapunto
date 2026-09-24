caf8b5e37d8b37d23ab8362d85ea579292a48036
fundidoamor <fundidoamor@gmail.com>
2026-09-23 23:44:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,29 @@ const previewPanel = document.querySelector('.preview-panel');
 const previews = document.querySelectorAll('.preview-placeholder');
 
 rows.forEach((row) => {
+
   row.addEventListener('mouseenter', () => {
+
     const previewId = row.dataset.preview;
-    previews.forEach((preview) => preview.classList.toggle('is-active', preview.id === previewId));
+
+    previews.forEach((preview) => {
+
+      preview.classList.toggle(
+        'is-active',
+        preview.id === previewId
+      );
+
+    });
+
     previewPanel.classList.add('is-visible');
+
   });
-  row.addEventListener('mouseleave', () => previewPanel.classList.remove('is-visible'));
+
+
+  row.addEventListener('mouseleave', () => {
+
+    previewPanel.classList.remove('is-visible');
+
+  });
+
 });
